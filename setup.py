79c440fbbcd5b1aca43aa4feb9e1f1b174b296e0
Christophe Demko <chdemko@gmail.com>
2020-03-31 14:06:21 +0200

    Merge

--- setup.py
+++ setup.py
@@ -17,104 +17,77 @@ here = path.abspath(path.dirname(__file__))
 # Get the long description from the README file
 try:
     import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
+
+    long_description = pypandoc.convert("README.md", "rst")
 except (IOError, ImportError):
-    with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    with open(path.join(here, "README.md"), encoding="utf-8") as f:
         long_description = f.read()
 
 setup(
-    name='pandoc-latex-color',
-
+    name="pandoc-latex-color",
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.0',
-
+    version="0.2.0",
     # The project's description
-    description='A pandoc filter for changing color in LaTeX',
+    description="A pandoc filter for changing color in LaTeX",
     long_description=long_description,
-
     # The project's main homepage.
-    url='https://github.com/chdemko/pandoc-latex-color',
-
+    url="https://github.com/chdemko/pandoc-latex-color",
     # The project's download page
-    download_url = 'https://github.com/chdemko/pandoc-latex-color/archive/master.zip',
-
+    download_url="https://github.com/chdemko/pandoc-latex-color/archive/master.zip",
     # Author details
-    author='Christophe Demko',
-    author_email='chdemko@gmail.com',
-
+    author="Christophe Demko",
+    author_email="chdemko@gmail.com",
     # Maintainer details
-    maintainer='Christophe Demko',
-    maintainer_email='chdemko@gmail.com',
-
+    maintainer="Christophe Demko",
+    maintainer_email="chdemko@gmail.com",
     # Choose your license
-    license='BSD-3-Clause',
-
+    license="BSD-3-Clause",
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
-
+        "Development Status :: 4 - Beta",
         # Specify the OS
-        'Operating System :: OS Independent',
-        
+        "Operating System :: OS Independent",
         # Indicate who your project is intended for
-        'Environment :: Console',
-        'Intended Audience :: End Users/Desktop',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        'Topic :: Software Development :: Documentation',
-        'Topic :: Text Processing :: Filters',
-
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Software Development :: Documentation",
+        "Topic :: Text Processing :: Filters",
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
-
     # What does your project relate to?
-    keywords='pandoc filters latex color',
-
+    keywords="pandoc filters latex color",
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
     py_modules=["pandoc_latex_color"],
-
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
-    entry_points={
-        'console_scripts': [
-            'pandoc-latex-color = pandoc_latex_color:main',
-        ],
-    },
-    
-    
+    entry_points={"console_scripts": ["pandoc-latex-color = pandoc_latex_color:main"]},
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[
-        'panflute>=1.10',
-        'pypandoc>=1.4'
-    ],
-
+    install_requires=["panflute<1.12", "pypandoc>=1.4"],
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
     # $ pip install -e .[dev,test]
-    extras_require={
-        'dev': ['check-manifest'],
-        'test': ['coverage'],
-    },
-
+    extras_require={"dev": ["check-manifest"], "test": ["coverage"]},
     # packages=find_packages(),
     # include_package_data = True,
-
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest', 'coverage'],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest", "coverage"],
 )
